cb4675d2b2805298fd5124be4b65c75fac4f18ef
cebrf <cebrf@yandex.ru>
2019-10-21 15:28:54 +1000

    add two basic pages and connection between them

---
+++ T1_water_delivery/T1_water_delivery/T1_water_delivery/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace T1_water_delivery
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            mainLabel.Text = "Astaroth";
+        }
+
+        private void goNext_Clicked(object sender, EventArgs e)
+        {
+            SelectionOfProduct newPage = new SelectionOfProduct();
+            Navigation.PushAsync(newPage);
+        }
+    }
+}
