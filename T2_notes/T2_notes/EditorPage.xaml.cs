32b629d018eb840e1ea62167206a42d89ce15bef
cebrf <cebrf@yandex.ru>
2019-12-17 00:44:29 +1000

    T2 add method addNote with working pan and left/right pushing in containers

---
+++ T2_notes/T2_notes/EditorPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace T2_notes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditorPage : ContentPage
+    {
+        public string text { private set; get; }
+        public DateTime timeChanged;
+
+        public EditorPage()
+        {
+            InitializeComponent();
+
+            timeChanged = DateTime.Now;
+            timeAndSymb.Text = timeChanged.ToString();
+            if (text != null)
+            {
+                timeAndSymb.Text += "    " + text.Length;
+            }
+        }
+
+        public EditorPage(DateTime timeChanged, string text)
+        {
+            InitializeComponent();
+            this.text = text;
+            textHolder.Text = text;
+            this.timeChanged = timeChanged;
+
+            timeAndSymb.Text = timeChanged.ToString();
+            if (text != null)
+            {
+                timeAndSymb.Text += "    " + text.Length;
+            }
+        }
+
+        private void textHolder_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            text = textHolder.Text;
+            timeChanged = DateTime.Now;
+        }
+    }
+}
--- T2_notes/T2_notes/MainPage.xaml.cs
+++ T2_notes/T2_notes/MainPage.xaml.cs
@@ -15,7 +15,6 @@ namespace T2_notes
     {
         protected void addNote(string text)
         {
-<<<<<<< HEAD
             DateTime timeChanged = DateTime.Now;
             
             Label newNote = new Label()
@@ -122,88 +121,6 @@ namespace T2_notes
 
                 string text = editorPage.text;
                 addNote(text);
-=======
-            InitializeComponent();
-
-
-            // add saved notes
-        }
-
-        private void add_Clicked(object sender, EventArgs e)
-        {
-            EditorPage editorPage = new EditorPage();
-            editorPage.Disappearing += (object editorPageSender, EventArgs editorPageargs) =>
-            {
-                if (editorPage.text == null)
-                    return;
-
-                string text = editorPage.text;
-                DateTime timeChanged = DateTime.Now;
-
-                Label newNote = new Label()
-                {
-                    Margin = new Thickness(0, 0, 0, 0),
-                    LineBreakMode = LineBreakMode.TailTruncation,
-                    Text = text
-                };
-                Frame newNoteFrame = new Frame()
-                {
-                    BackgroundColor = Color.Bisque,
-                    BorderColor = Xamarin.Forms.Color.DarkSeaGreen,
-                    Margin = new Thickness(0, 0, 0, 0),
-                    Padding = new Thickness(5, 5, 0, 5),
-                    Content = newNote
-                };
-
-                var pan = new PanGestureRecognizer();
-                double totalX = 0;
-                pan.PanUpdated += async (panSender, panArgs) =>
-                {
-                    switch (panArgs.StatusType)
-                    {
-                        case GestureStatus.Canceled:
-                        case GestureStatus.Started:
-                            newNoteFrame.TranslationX = 0;
-                            break;
-                        case GestureStatus.Completed:
-                            if (totalX > 0)  // < 0 for left
-                            {
-                                if (await DisplayAlert("Confirm the deleting", "Are you sure?", "Yes!", "No"))
-                                {
-                                    rightContainer.Children.Remove(panSender as Frame);
-                                }
-                                totalX = 0;
-                            }
-                            newNoteFrame.TranslationX = 0;
-                            break;
-                        case GestureStatus.Running:
-                            if (panArgs.TotalX > 0)  // < 0 for left
-                            {
-                                newNoteFrame.TranslationX = panArgs.TotalX;
-                                totalX = panArgs.TotalX;
-                            }
-                            break;
-                    }
-                };
-                newNoteFrame.GestureRecognizers.Add(pan);
-                rightContainer.Children.Add(newNoteFrame);
-
-                
-
-                var tap = new TapGestureRecognizer();
-                tap.Tapped += (tapSender, tapEven) =>
-                {
-                    EditorPage editorPage_ = new EditorPage(timeChanged, text);
-                    Navigation.PushAsync(editorPage_);
-                    editorPage_.Disappearing += (_, __) =>
-                    {
-                        text = editorPage_.text;
-                        newNote.Text = text;
-                        timeChanged = editorPage_.timeChanged;
-                    };
-                };
-                newNote.GestureRecognizers.Add(tap);
->>>>>>> 47d4d6e8c9fe0d9bda0ceed8e9a252d0260bed1b
             };
             Navigation.PushAsync(editorPage);
         }
