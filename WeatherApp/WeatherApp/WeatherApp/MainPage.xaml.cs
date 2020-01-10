1cc1ce0228850f93c57fd967c0adc688e21b729d
cebrf <cebrf@yandex.ru>
2020-01-10 23:48:19 +1000

    Weather fix WeatherData

--- WeatherApp/WeatherApp/WeatherApp/MainPage.xaml.cs
+++ WeatherApp/WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -28,6 +28,7 @@ namespace WeatherApp
             //TODO load chosen city from json
             restService = new RestService();
             getDayWeather(App.chosenCities[0]);
+            getForecast(App.chosenCities[0]);
         }
 
         private void ChooseCity_Clicked(object sender, EventArgs e)
@@ -39,6 +40,7 @@ namespace WeatherApp
                 if (chosenCity != null)
                 {
                     getDayWeather(chosenCity);
+                    getForecast(chosenCity);
 
                     // TODO get info about city
 
@@ -82,71 +84,3 @@ namespace WeatherApp
         }
     }
 }
-
-
-/*
- public MainPage()
-        {
-            InitializeComponent();
-
-            PageWeather = new MainPageWeather();
-            App.chosenCities = new List<string>() { "Paris", "Moscow", "London", "Vladivostok, RU" };
-            //TODO load chosen city from json
-            restService = new RestService();
-            getDayWeather(App.chosenCities[0]);
-            //getForecast(App.chosenCities[0]);
-        }
-
-        private void ChooseCity_Clicked(object sender, EventArgs e)
-        {
-            ChooseCityPage cityPage = new ChooseCityPage();
-            cityPage.Disappearing += (object cityPageSender, EventArgs cityPageargs) =>
-            {
-                var chosenCity = cityPage.chosenCity;
-                if (chosenCity != null)
-                {
-                    getDayWeather(chosenCity);
-                    //getForecast(chosenCity);
-
-                    // TODO get info about city
-
-                    //TODO save chosen city to json
-                }
-
-            };
-            Navigation.PushAsync(cityPage);
-        }
-        async void getDayWeather(string cityName)
-        {
-            WeatherData weatherData = await restService.GetWeatherData(GenerateRequestUri(Constants.OpenWeatherMapEndpoint, cityName));
-            
-            PageWeather.WeatherData = weatherData;
-            MainPageWeather newWether = new MainPageWeather()
-            {
-                ForecastData = PageWeather.ForecastData,
-                WeatherData = PageWeather.WeatherData
-            };
-            BindingContext = PageWeather;
-        }
-
-        async void getForecast(string cityName)
-        {
-            ForecastData forecastData = await restService.GetForecastData(GenerateRequestUri(Constants.OpenWeatherMapForecastEndpoint, cityName));
-            PageWeather.ForecastData = forecastData;
-            MainPageWeather newWether = new MainPageWeather() 
-            { 
-                ForecastData = PageWeather.ForecastData,
-                WeatherData = PageWeather.WeatherData
-            };
-            BindingContext = newWether;
-        }
-
-string GenerateRequestUri(string endpoint, string cityName)
-{
-    string requestUri = endpoint;
-    requestUri += $"?q={cityName}";
-    requestUri += "&units=metric"; // or units=imperials
-    requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
-    return requestUri;
-}
-     */
